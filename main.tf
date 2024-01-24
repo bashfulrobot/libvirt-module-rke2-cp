2bf2585dc0f71d8d45d0ce9ff6454fad8233c566
bashfulrobot <dustin@bashfulrobot.com>
2024-01-24 12:22:59 -0800

    chore: reorder runcmd, start rke2 cp install script
    - run: install-rke2-cp.sh
    Changes to be committed:
    	modified:   main.tf

--- main.tf
+++ main.tf
@@ -74,8 +74,9 @@ runcmd:
   - [ bash, -c, 'start_time=$(TZ=":America/Vancouver" date "+%Y-%m-%d %H:%M:%S.%N %Z"); echo "Running Wget - install-rke2-cp.sh: $start_time" >> /home/${var.admin_name}/cloud-init-run.log' ]
   - [ bash, -c, 'wget -P /tmp https://raw.githubusercontent.com/bashfulrobot/libvirt-module-helpers/main/install-rke2-cp.sh >> /home/${var.admin_name}/cloud-init-run.log 2>&1' ]
   - [ bash, -c, 'start_time=$(TZ=":America/Vancouver" date "+%Y-%m-%d %H:%M:%S.%N %Z"); echo "Running Wget - miniserve: $start_time" >> /home/${var.admin_name}/cloud-init-run.log' ]
-  - [ bash, -c, 'wget -P /tmp https://raw.githubusercontent.com/bashfulrobot/libvirt-module-helpers/main/miniserve >> /home/${var.admin_name}/cloud-init-run.log 2>&1' ]
   - [ bash, -c, 'chmod +x /tmp/miniserve /tmp/install-rke2-cp.sh >> /home/${var.admin_name}/cloud-init-run.log 2>&1' ]
+  - [ bash, -c, 'wget -P /tmp https://raw.githubusercontent.com/bashfulrobot/libvirt-module-helpers/main/miniserve >> /home/${var.admin_name}/cloud-init-run.log 2>&1' ]
+  - [ bash, -c, '/tmp/install-rke2-cp.sh >> /home/${var.admin_name}/cloud-init-run.log 2>&1' ]
   - [ bash, -c, 'echo "Cloud-init end: $(TZ=":America/Vancouver" date "+%Y-%m-%d %H:%M:%S.%N %Z")" >> /home/${var.admin_name}/cloud-init-run.log' ]
 EOF
 }
